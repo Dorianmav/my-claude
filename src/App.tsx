a4688e508a99fa1ab85a409350c1f7561a140602
Dorian MAVOUNGOUD <dorian.mavoungoud@milleis.fr>
2025-02-06 10:34:38 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl">Mia Claude</h1>
+        <p>Initialisation du projet avec TS React et Tailwind</p>
+      </div>
+    </>
+  )
+}
+
+export default App
